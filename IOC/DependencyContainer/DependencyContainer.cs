2569e736b3ac176ec06a70a51bfba15f4b9b73a1
dev-mohsen-naserian <145559489+dev-mohsen-naserian@users.noreply.github.com>
2024-10-24 15:58:58 -0700

    improve Layer IOC

---
+++ IOC/DependencyContainer/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC.DependencyContainer;
+
+public static class DependencyContainer
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        #region Repository
+      //  services.AddKeyedScoped<>();
+        #endregion
+        #region Service
+        #endregion
+    }
+}
