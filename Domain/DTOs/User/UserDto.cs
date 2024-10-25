960c7650bc9f1a90f739c7ef4a206aca00fa09e0
dev-mohsen-naserian <145559489+dev-mohsen-naserian@users.noreply.github.com>
2024-10-25 02:17:36 -0700

    Add User Entity and DTO for Authentication

---
+++ Domain/DTOs/User/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.User;
+
+public class UserDto
+{
+    public string UserName { get; set; }
+    public string DisplayName { get; set; }
+    public string Avatar { get; set; }
+    public string Token { get; set; }
+}
---
+++ Domain/Entities/User/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.User;
+
+public class UserEntity
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public bool IsEmailActive { get; set; }
+    public string Mobile { get; set; }
+    public string Avatar { get; set; }
+    public string DisplayName { get; set; }
+    public DateTime RegisterDate { get; set; }
+}
