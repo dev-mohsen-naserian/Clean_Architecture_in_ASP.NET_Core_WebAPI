7222a58f4b4840bff60b98e47f2835109681408c
dev-mohsen-naserian <145559489+dev-mohsen-naserian@users.noreply.github.com>
2024-10-26 01:17:32 -0700

    add Authentication

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,19 +1,39 @@
 using IOC.DependencyContainer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using WebApi.Services.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+#region services
 builder.Services.RegisterServices();
 builder.Services.AddScoped<ITokenService,ITokenService>();
+#endregion
+#region Config Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]));
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = key,
+        ValidateIssuer = false,
+        ValidateAudience=false
+    };
+});
+#endregion
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
